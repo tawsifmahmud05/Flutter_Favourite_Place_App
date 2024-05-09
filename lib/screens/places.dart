1bf40df27e1e3a971ec92a49d577a51614d01f63
Tawsif05 <tawsifmahmud05@gmail.com>
2024-05-10 00:10:23 +0600

    uploaded

---
+++ lib/screens/places.dart
@@ -0,0 +1,36 @@
+import 'package:favourite_place/providers/user_places.dart';
+import 'package:favourite_place/screens/add_place.dart';
+import 'package:favourite_place/widgets/places_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PlacesScreen extends ConsumerWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Places"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (ctx) => const AddPlaceScreen(),
+              ));
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: PlacesList(
+          places: userPlaces,
+        ),
+      ),
+    );
+  }
+}
