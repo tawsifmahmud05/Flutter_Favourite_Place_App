7c8e84e766e01672e7d98bf80a4082e889f3e834
Tawsif05 <tawsifmahmud05@gmail.com>
2024-05-10 16:03:51 +0600

    google map initialize

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:favourite_place/models/place.dart';
 import 'package:favourite_place/providers/user_places.dart';
 import 'package:favourite_place/widgets/image_input.dart';
 import 'package:favourite_place/widgets/location_input.dart';
@@ -18,17 +19,21 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
   File? _selectedImage;
+  PlaceLocation? _selectedLocation;
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if (enteredTitle.isEmpty || _selectedImage == null) {
+    if (enteredTitle.isEmpty ||
+        _selectedImage == null ||
+        _selectedLocation == null) {
       return;
     }
 
     ref.read(userPlacesProvider.notifier).addPlace(
           enteredTitle,
           _selectedImage!,
+          _selectedLocation!,
         );
 
     Navigator.of(context).pop();
@@ -64,7 +69,11 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               },
             ),
             const SizedBox(height: 10),
-            LocationInput(),
+            LocationInput(
+              onSelectLocation: (location) {
+                _selectedLocation = location;
+              },
+            ),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -6,6 +6,7 @@ class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
 
   final List<Place> places;
+
   @override
   Widget build(BuildContext context) {
     if (places.isEmpty) {
@@ -33,6 +34,12 @@ class PlacesList extends StatelessWidget {
               .textTheme
               .titleMedium!
               .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+        subtitle: Text(
+          places[index].location.address,
+          style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
         ),
         onTap: () {
           Navigator.of(context).push(
